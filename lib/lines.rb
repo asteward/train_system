661ba88a86ca14c87eca01c0e73d26ca2e2994ed
Austin Steward <austinsteward@aol.com>
2014-08-11 03:40:03 +0000

    List train lines in database

--- lib/lines.rb
+++ lib/lines.rb
@@ -1,5 +1,5 @@
 class Line
-  attr_reader :id, :name
+  attr_accessor :id, :name
   def initialize(line_info)
     @id = line_info['id']
     @name = line_info['name']
@@ -8,5 +8,14 @@ class Line
   def save
     results = DB.exec("INSERT INTO line (name) VALUES ('#{@name}') RETURNING id;").first
     @id = results['id'].to_i
+  end
+
+  def self.all
+    lines =[]
+    results = DB.exec("SELECT * FROM line;")
+    results.each do |result|
+      lines << Line.new({:id => result['id'].to_i, :name => result['name']})
+    end
+    lines
   end
 end
--- spec/lines_spec.rb
+++ spec/lines_spec.rb
@@ -15,4 +15,14 @@ describe 'Line' do
       expect(Line.all).to eq [new_line]
     end
   end
+
+  describe '.all' do
+    it 'returns an array of all the train lines in the database' do
+      new_line = Line.new({:name => "15th ST Express"})
+      new_line.save
+      new_line2 = Line.new({:name => "HWY 99 Line"})
+      new_line2.save
+      expect(Line.all).to eq [new_line, new_line2]
+    end
+  end
 end
