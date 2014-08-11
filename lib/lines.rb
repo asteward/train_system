7ac19345017490f8e78bd84c2baefce6090f4c4b
Austin Steward <austinsteward@aol.com>
2014-08-11 03:25:04 +0000

    Save train line to database

--- lib/lines.rb
+++ lib/lines.rb
@@ -4,4 +4,9 @@ class Line
     @id = line_info['id']
     @name = line_info['name']
   end
+
+  def save
+    results = DB.exec("INSERT INTO line (name) VALUES ('#{@name}') RETURNING id;").first
+    @id = results['id'].to_i
+  end
 end
--- spec/lines_spec.rb
+++ spec/lines_spec.rb
@@ -7,4 +7,12 @@ describe 'Line' do
       expect(new_line).to be_a Line
     end
   end
+
+  describe 'save' do
+    it 'saves a line to the database' do
+      new_line = Line.new({:name => "15th ST Express"})
+      new_line.save
+      expect(Line.all).to eq [new_line]
+    end
+  end
 end
